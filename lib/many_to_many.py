6744f83fc1d912d77f02b38e7fc88be973c7e61f
Jonathan Marshall <jwmarsh16@gmail.com>
2024-07-02 18:17:23 -0500

    lab complete

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -3,39 +3,38 @@ class Author:
         self.name = name
         self._contracts = []
         
-
-    def get_contracts(self, contract):
-        if isinstance(contract, Contract):
-            self.related_contracts.append(contract)
-
-
-    def books(self, book):
-        if isinstance(book, Book):
-            self.related_books.append(book)
-
-    def total_royalties(self):
-        for contract in self.related_contracts:
-            self.royalties += contract.royalties
-        return self.royalties
-
-    def sign_contract(book, date, royalties):
-        if isinstance(book, Book) and isinstance(date, str) and isinstance(royalties, int):
-            self.royalties += royalties
-            return Contract(self, book, date, royalties)
-        else:
-            return None
-    
         
-    pass
+
+    def contracts(self):
+        return self._contracts
+            
+
+    def books(self):
+        return list({contract.book for contract in self._contracts})
+
+
+    def sign_contract(self, book, date, royalties):
+        contract = Contract(self, book, date, royalties)
+        return contract
+    
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self._contracts)
 
 
 class Book:
     def __init__(self, title):
         self.title = title
-    pass
+        self._contracts = []
+
+    def contracts(self):
+        return self._contracts
+    
+    def authors(self):
+        return list({contract.author for contract in self._contracts})
 
 
 class Contract:
+    all = []
     def __init__(self, author, book, date, royalties):
         if not isinstance(author, Author):
             raise TypeError("author must be an instance of Author")
@@ -45,16 +44,16 @@ class Contract:
             raise TypeError("date must be a string")
         if not isinstance(royalties, int):
             raise TypeError("royalties must be an integer")
-        
+    
         self.author = author
         self.book = book
         self.date = date
         self.royalties = royalties
 
+        author._contracts.append(self)
+        book._contracts.append(self)
+        Contract.all.append(self)
 
+    @classmethod
     def contracts_by_date(cls, date):
-        for contract in cls.related_contracts:
-            if contract.date == date:
-                return contract
-
-    pass
+        return [contract for contract in cls.all if contract.date == date]
